5ffd3d8e65864bc9a1892af9fc4abe844912d7f6
eqsdxr <rxdsqe@gmail.com>
2025-04-01 19:12:09 +0500

    print temperature conversion table in reverse order

---
+++ 1-5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+  printf("------------------------\n");
+  printf("| Fahrenheit | Celsius |\n");
+  printf("------------------------\n");
+
+  for (int fahr = 300; fahr >= 0; fahr = fahr - 20) {
+    printf("        %4.f      %4.1f\n", (float) fahr, (9.0/5.0 * fahr + 32));
+  }
+}
