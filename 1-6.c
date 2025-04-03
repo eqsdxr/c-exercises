bd9b45d8ed6590ec17cba8b9885770de45df013e
eqsdxr <rxdsqe@gmail.com>
2025-04-03 16:00:44 +0500

    check that expression "getchar() != EOF" is 0 or 1

---
+++ 1-6.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+
+  // Use Ctrl+D to stop execution properly if you use bash
+
+  printf("%d\n", (getchar() != EOF));
+ 
+  return EXIT_SUCCESS;
+}
