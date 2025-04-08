b17cbed937989c907c8dbfb434ce604c3c8c1203
eqsdxr <rxdsqe@gmail.com>
2025-04-08 13:02:12 +0500

    replace each string of one or more blanks by a single blank

---
+++ 1-9.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define YES 1
+#define NO 0
+
+int main() {
+  int c;
+  int flag;
+
+  flag = NO;
+
+  while ((c = getchar()) != EOF) {
+    if (c != ' ' && flag == YES) {
+      flag = NO;
+      putchar(c);
+    } else if (c == ' ' && flag == NO) {
+      flag = YES;
+      putchar(c);
+    } else if (c == ' ' && flag == YES)
+      ;
+    else
+      putchar(c);
+  }
+  return EXIT_SUCCESS;
+}
