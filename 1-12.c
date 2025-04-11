94707e88b32e2dcf923a34e72131a5f2cee3fa10
eqsdxr <rxdsqe@gmail.com>
2025-04-11 17:06:15 +0500

    print one word per line

---
+++ 1-12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define YES 1
+#define NO 0
+
+int main() {
+  int c;
+  while ((c = getchar()) != EOF) {
+    if (c != ' ' && c != '\t' && c != '\n' && c != '\b')
+      putchar(c);
+    else
+      putchar('\n');
+  }
+  return EXIT_SUCCESS;
+}
