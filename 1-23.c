db4b88dce7f9d94e26dcaa26f217cb079adf964e
eqsdxr <rxdsqe@gmail.com>
2025-04-30 21:47:21 +0500

    minor changes

--- 1-23.c
+++ 1-23.c
@@ -1,6 +1,6 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 #define MAXLEN 1000
 
@@ -23,30 +23,30 @@ bool remove_multiple_line_comments(char s[], int len) {
   int i = 0;
 
   while (s[i] != '\0') {
-    if (s[i] == '/' && s[i+1] == '*') {
-        if (i > 0 && s[i-1] == '\\')
-          continue;
-        else {
-          s[i] = '\0';
-          return true;
-        }
-    } else if (s[i] == '*' && s[i+1] == '/') {
-        if (i > 0 && s[i-1] == '\\')
-          continue;
-        char reminder[len-i+1];
-        int m = i;
-        for (int j = 0; j < len-m; ++j) {
-          reminder[j] = s[m];
-          ++m;
-        }
-        reminder[m] = '\0';
-
-        for (int j = 0; j < len-i; ++j) {
-          s[i] = reminder[j];
-          ++i;
-        }
-        return false;
+    if (s[i] == '/' && s[i + 1] == '*') {
+      if (i > 0 && s[i - 1] == '\\')
+        continue;
+      else {
+        s[i] = '\0';
+        return true;
       }
+    } else if (s[i] == '*' && s[i + 1] == '/') {
+      if (i > 0 && s[i - 1] == '\\')
+        continue;
+      char reminder[len - i + 1];
+      int m = i;
+      for (int j = 0; j < len - m; ++j) {
+        reminder[j] = s[m];
+        ++m;
+      }
+      reminder[m] = '\0';
+
+      for (int j = 0; j < len - i; ++j) {
+        s[i] = reminder[j];
+        ++i;
+      }
+      return false;
+    }
     ++i;
   }
   return false;
@@ -60,14 +60,15 @@ void remove_one_line_comments(char s[], int len) {
   while (s[i] != '\0') {
     if (s[i] == '"' || s[i] == '\'' && in_quotes == false) {
       in_quotes = true;
-    } else if (s[i] == '"' || s[i] == '\'' && in_quotes == true && s[i-1] != '\\') {
+    } else if (s[i] == '"' ||
+               s[i] == '\'' && in_quotes == true && s[i - 1] != '\\') {
       in_quotes = false;
-    } else if (s[i] == '/' && s[i+1] == '/') {
+    } else if (s[i] == '/' && s[i + 1] == '/') {
       if (in_quotes == true)
         continue;
       else {
-          s[i] = '\0';
-          break;
+        s[i] = '\0';
+        break;
       }
     }
     ++i;
